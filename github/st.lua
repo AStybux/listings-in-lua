308369b4bdd7700813687060288bc5821558a73d
AStybux <95493415+AStybux@users.noreply.github.com>
2025-11-23 02:48:52 +0300

    Update st.lua
    big upgrade code for starting terminal
    for run you need
    |>figlet
    |>fortune
    |>fingers
    |>lua
    And possible git bash because in windows this very bad work(btw you can read about linux hehe)

--- github/st.lua
+++ github/st.lua
@@ -1,47 +1,111 @@
-local function ex(text)
-    os.execute(text)
-end
+local coolf = ("").format
 
-fd = io.popen('hostname')
-result = fd:read('*all')
-
---ex("cd && love l/lv/soo && clear")
--- ex("
-local function r()
-    return math.floor(os.clock()*20000)%2
-end
-
-function sleep(n)
-    os.execute("sleep " .. tonumber(n))
-end
-
-
--- if result == "terminal" then
---     print(1)
--- end
-
--- print(string.len(result))
--- print(string.len("terminal\n"))
-
-
-if r()==1 then
-    if result == "terminal\n" then
-        ex("screenfetch -c 01,8")
+local function terminal(command)
+    if command ~= nil then
+        os.execute(command)
     else
-        -- ex("echo 'Выполнение команды...'")
-        ex("sudo hostname terminal")
-        -- ex("echo '\nУспешно!'" )
-        ex("screenfetch -c 01,8")
-    end
-else
-    sleep(1)
-    if r()==1 then
-        ex("cpufetch --color 208,70,72:210,170,153:1,1,1:68,36,52:210,170,153 --logo-intel-new")
-    else
-        ex("cpufetch --logo-long --color 208,70,72:210,170,153:1,1,1:68,36,52:210,170,153")
+        os.execute("clear")
     end
 end
 
--- вывести последние команды 
--- cpufetch --logo-long --color 208,70,72:210,170,153:1,1,1:68,36,52:210,170,153
---color intel
+local function popen(command)
+    if command then
+        local handle = io.popen(command)
+        local result = handle:read("*a")
+
+        handle:close()
+
+        return result
+    else
+
+        return [[
+Программа           Слова
+bastet              тетрис, стратегические игры, логика, развлечение
+baobab              очистка, управление, файловая система, сервисы для каталогизации
+cmus                музыка, аудиоплеер, воспроизведение, плейлисты, поддержка форматов
+catimg / chafa      вывод картинки на экран терминала, ASCII, графика, отображение изображений
+cmatrix             матрица, визуализация, экранная заставка, анимация
+di                  диск, файловая система, управление дисками, информация о хранилище
+dosbox              архитектура, эмуляция, винтажные игры, старые программы, DOS
+eog                 фото, просмотр изображений, поддержка форматов, редактор
+exa                 содержимое, файловая система, альтернативная команда ls, интеграция с Git
+        ]]
+    end
+end
+
+local function generate_printf_commands()
+    local text_block = popen(nil)
+    local is_header_passed = false
+
+    for line in text_block:gmatch("[^\r\n]+") do
+        local _, words_part = line:match("^(.-)%s+(.*)$")
+        if words_part then
+            for word in words_part:gmatch("[^,]+") do
+                terminal(coolf('printf "\\033[90m%s\\033[0m"', word:match("^%s*(.-)%s*$")))
+            end
+        end
+    end
+end
+
+local function fortune(n)
+    local n = n or 5
+    local output = popen "fortune ru"
+
+    if #output < 23*11 then
+
+        return output
+    elseif n > 0 then
+
+        return fortune(n - 1)
+    else
+
+        return output
+    end
+end
+
+local function XY(x, y, text)
+    local i = 0
+
+    for line in (("").format("%s\n",text)):gmatch("(.-)\n") do
+        i = i + 1
+        io.write(coolf("\27[%d;%dH%s\n", y + i, x, line))
+    end
+end
+
+terminal()
+
+XY(0,2, popen("figlet --font mono9 --metal lua"))
+
+local function popen_with_gray_column()
+    local base_table = popen(nil)
+    local result_lines = {}
+    local header_passed = false
+
+    for line in base_table:gmatch("[^\r\n]+") do
+        if not header_passed then
+            local col1, col2 = line:match("^(.-)%s%s+(.+)$")
+
+            if col1 and col2 then
+                table.insert(result_lines, coolf("%-20s\t    \27[90m%s\27[0m", col1, col2))
+            else
+                table.insert(result_lines, line)
+            end
+            header_passed = true
+        else
+            local program, words = line:match("^(.-)%s%s+(.+)$")
+
+            if program and words then
+                local colored_line = coolf("%-20s\27[90m%s\27[0m", program, words)
+
+                table.insert(result_lines, colored_line)
+            else
+                table.insert(result_lines, line)
+            end
+        end
+    end
+    
+    return table.concat(result_lines, "\n")
+end
+
+XY(25, 1, popen_with_gray_column())
+XY(0, 13, fortune())
